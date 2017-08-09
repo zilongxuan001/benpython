8c4d19a5e46706fa65e46966f22db5139fd0f3d1
zilongxuan001 <zilongxuan001@163.com>
2017-08-09 18:48:36 +0800

    add ex01.py

---
+++ ex01.py
@@ -0,0 +1,16 @@
+# -- coding: utf-8 --
+
+#date:20170809.
+
+print('Hello world!')
+print('Hello Again')
+print('I like typing this')
+print('This is fun.')
+print('Yay! Printing.')
+print('I\'d much rather you \'not\'.')
+print('I \"said\" do not touch this.')
+
+#add grade
+print('I love python.\nI like to use python.')
+print('''Just try.
+You can do it.''')
